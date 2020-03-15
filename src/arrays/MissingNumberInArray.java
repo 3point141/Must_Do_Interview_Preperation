dad9160e08067174f3ea3ec960692e9a362495ff
amank <amank.ak.kumar@oracle.com>
2020-03-15 18:43:36 +0530

    arrays

---
+++ src/arrays/MissingNumberInArray.java
@@ -0,0 +1,59 @@
+package arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * Given an array C of size N-1 and given that there are numbers from 1 to N with one element missing, the missing number is to be found.
+ *
+ * Input:
+ * The first line of input contains an integer T denoting the number of test cases. For each test case first line contains N(size of array).
+ * The subsequent line contains N-1 array elements.
+ *
+ * Output:
+ * Print the missing number in array.
+ *
+ * Constraints:
+ * 1 ≤ T ≤ 200
+ * 1 ≤ N ≤ 107
+ * 1 ≤ C[i] ≤ 107
+ *
+ * Example:
+ * Input:
+ * 2
+ * 5
+ * 1 2 3 5
+ * 10
+ * 1 2 3 4 5 6 7 8 10
+ *
+ * Output:
+ * 4
+ * 9
+ *
+ * Explanation:
+ * Testcase 1: Given array : 1 2 3 5. Missing element is 4.
+ */
+
+
+public class MissingNumberInArray {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(reader.readLine());
+        while (T-- > 0) {
+            int N = Integer.parseInt(reader.readLine());
+            String []input = reader.readLine().split(" ");
+            int sum = (N*(N+1))/2;
+
+            for(int i=0; i<N-1; i++) {
+                sum-= Integer.parseInt(input[i]);
+            }
+
+            System.out.println(sum);
+        }
+    }
+}
